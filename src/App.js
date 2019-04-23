0974df4f8e6201aaa9c9ec7afe41a5778323ccd7
Issam Ait Ouahmane <49284629+issam1994@users.noreply.github.com>
2019-04-23 18:42:13 +0100

    Code refactored and simplified
    I refactored the code according to SRP. It's now simpler & readable.
    
    Now there are two functions that fetch data and they are argumentless, one loads only the 1st page and is called only once, the other one loads the next pages and it's called every time the user scrolls but loads new data only when the user reaches the end of the page.
    One more thing, the scroll event listener is now added only when the 1st page is finished loading, this is a better implementation compared to before when it was added through the life cycle method componentDidMount. Happy reviewing...

--- src/App.js
+++ src/App.js
@@ -3,99 +3,82 @@ import './App.css';
 import RepoContainer from './components/repoContainer'
 
 class App extends Component {
-    
     state = {
         repos: [],
-        isLoading: true,
-        page: 1
+        isLoadingFirstPage: true
     }
-    // this function returns today's date minus 1 month
-goBack30 = (today) => {
-//    console.log(today)
-    let year = today.getFullYear();
-    let month = today.getMonth();
-    let day = today.getDate();
-    return `${year}-${month < 9 && month !== 0 ? "0" + (month): month === 0 ? 12  : month }-${day < 10 ? "0" + day: day}`
-}
-    // depending on the string passed, this function updates state with new repos
-loadData = (str) => {
-    let today = new Date();
-    let last30Days = this.goBack30(today);
-    let pageNumber = this.state.page;
-    let isLoading = this.state.isloading;
-    // on the first run of the app we load the first page of repos
-    if (str === "first load"){
-        fetch(`https://api.github.com/search/repositories?q=created:>${last30Days}&sort=stars&order=desc`).then(res => res.json())
-        .then(data => {
-        const repos = data.items;
-//        console.log(repos)
-        this.setState({
-        repos: repos,
-        isLoading: false
-        })  
-    })
-    }
-    // on the next load, the app fetches the next page of repos to update state by adding more repos and saving the current page number
-    else if (str === "next load" && !isLoading){
-        fetch(`https://api.github.com/search/repositories?q=created:>${last30Days}&sort=stars&order=desc${"&page=" + (pageNumber + 1)}`).then(res => res.json())
-        .then(data => {
-        let currentRepos = this.state.repos;
-        let NextRepos = data.items;
-//        console.log(repos)
-        this.setState({
-        repos: currentRepos.concat(NextRepos),
-        page: pageNumber + 1
-        })
-        })
-    }
-}
-    // when a user scrolls down to the end, this function is triggered and calls LoadData to fetch the next page of repos
-loadMore = () => {
-//    console.log("scrolling");
-    if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight){
-        this.loadData("next load");
-//        console.log("end of the line!");
-      }
-}
-    // in this lifecycle method we load the first page of repos by calling loadData() and passing the string "first load"
+////////////////////
 componentWillMount(){
-    
-    this.loadData("first load");
-    
-    
+    this.loadFirstPage();
 }
-    // listening to scroll event
-componentDidMount(){
-    window.addEventListener("scroll", () => this.loadMore())
+/////////////////////
+oneMonthBeforeToday = () => {
+    let currentDate = new Date();
+    let year = currentDate.getFullYear();
+    let month = currentDate.getMonth();
+    let day = currentDate.getDate();
+    let dateString = `${year}-${month < 9 && month !== 0 ? "0" + (month): month === 0 ? 12  : month }-${day < 10 ? "0" + day: day}`
+    return dateString
 }
-    // removing the event listener when the component unmounts {a good practice}
+//////////////////////
+loadFirstPage = () => {
+    let firstPageUrl = `https://api.github.com/search/repositories?q=created:>${this.oneMonthBeforeToday()}&sort=stars&order=desc`
+    fetch(firstPageUrl)
+        .then(res => res.json())
+            .then(data => {
+                const repos = data.items;
+                this.setState({
+                        repos: repos,
+                        isLoadingFirstPage: false,
+                        currentPage: 1
+                    });
+                this.listenToScrolling();
+        })
+}
+/////////////////////
+listenToScrolling = () => window.addEventListener("scroll", this.loadNextPage);
+///////////////////////
+loadNextPage = () => {
+    if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight){
+        let currentPage = this.state.currentPage
+        let nextPageUrl = `https://api.github.com/search/repositories?q=created:>${this.oneMonthBeforeToday()}&sort=stars&order=desc${"&page="+(currentPage + 1)}`
+        fetch(nextPageUrl)
+            .then(res => res.json())
+                .then(data => {
+                    let {repos} = this.state;
+                    let nextPageRepos = data.items;
+                    this.setState({
+                            repos: repos.concat(nextPageRepos),
+                            currentPage: currentPage + 1
+                        })
+                })
+    }
+}
+///////////////////////
 componentWillUnmount(){
-    window.removeEventListener("scroll", () => this.loadMore());
+    window.removeEventListener("scroll", this.loadNextPage);
 }
-
+//////////////////////
   render() {
-      
-    const reposArr = this.state.repos;
+    const {isLoadingFirstPage, repos} = this.state
     const converToDays = 1000*60*60*24;
     const converToHours = 1000*60*60;
     const converToMinutes = 1000*60;
-    let isLoading = this.state.isLoading
-    let today = new Date();
+    const today = new Date();
     
     return (
       <div className="App">
-        <h1 id="title">Trending Repos</h1>
-        {isLoading? "Loading.." :reposArr.map( repo =>
+        {isLoadingFirstPage? "Loading.." :repos.map( repo =>
         <RepoContainer
-        avatar = {repo.owner["avatar_url"]}
-        name = {repo.name}
-        description = {repo.description}
-        stars = {repo["stargazers_count"] > 1000? (repo["stargazers_count"]/ 1000).toFixed(2) + "k":repo["stargazers_count"]}
-        issues = {repo["open_issues_count"] > 1000? (repo["open_issues_count"]/ 1000).toFixed(2) + "k":repo["open_issues_count"]}
-        // this part was really fun :D!
-        timeInterval = {parseInt((today - new Date(repo["pushed_at"])) / converToHours) >= 24 ? parseInt((today - new Date(repo["pushed_at"])) / converToDays) + " day(s) ago": parseInt((today - new Date(repo["pushed_at"])) / converToMinutes) >= 60 ? parseInt((today - new Date(repo["pushed_at"])) / converToHours) + " hour(s) ago": parseInt((today - new Date(repo["pushed_at"])) / converToMinutes) + " minute(s) ago"}
-        userName = {repo.owner.login}
-        key = {repo.id}
+            avatar = {repo.owner["avatar_url"]}
+            name = {repo.name}
+            description = {repo.description}
+            stars = {repo["stargazers_count"] > 1000? (repo["stargazers_count"]/ 1000).toFixed(1) + "k":repo["stargazers_count"]}
+            issues = {repo["open_issues_count"] > 1000? (repo["open_issues_count"]/ 1000).toFixed(1) + "k":repo["open_issues_count"]}
+            // this part was really fun :D!
+            timeInterval = {parseInt((today - new Date(repo["pushed_at"])) / converToHours) >= 24 ? parseInt((today - new Date(repo["pushed_at"])) / converToDays) + " day(s) ago": parseInt((today - new Date(repo["pushed_at"])) / converToMinutes) >= 60 ? parseInt((today - new Date(repo["pushed_at"])) / converToHours) + " hour(s) ago": parseInt((today - new Date(repo["pushed_at"])) / converToMinutes) + " minute(s) ago"}
+            userName = {repo.owner.login}
+            key = {repo.id}
         />)}
       </div>
     );
