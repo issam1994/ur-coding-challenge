1d67012e70bcde31b342ef408ea423f44b755372
Issam Ait Ouahmane <49284629+issam1994@users.noreply.github.com>
2019-06-08 19:11:38 +0000

    refactored the "time" algorithm & more
    refactored the part that decides in what unit to show the time passed since the last time a repo got updated, in functional programming style.
    Encapsulated the condition, that checks whether the user has reached the the end of the list or not, inside a function (scrolledToTheEndOfList).
    plus some minor fixes...

--- src/App.js
+++ src/App.js
@@ -1,89 +1,161 @@
-import React, { Component } from 'react';
-import './App.css';
-import RepoContainer from './components/repoContainer'
+import React, { Component } from "react";
+import "./App.css";
+import RepoContainer from "./components/repoContainer";
 
 class App extends Component {
-    state = {
-        repos: [],
-        isLoadingFirstPage: true
-    }
-////////////////////
-componentWillMount(){
+  state = {
+    repos: [],
+    isLoadingFirstPage: true
+  };
+  //
+  componentWillMount() {
     this.loadFirstPage();
-}
-/////////////////////
-oneMonthBeforeToday = () => {
+  }
+  //
+  oneMonthBeforeToday = () => {
     let currentDate = new Date();
     let year = currentDate.getFullYear();
     let month = currentDate.getMonth();
     let day = currentDate.getDate();
-    let dateString = `${year}-${month < 9 && month !== 0 ? "0" + (month): month === 0 ? 12  : month }-${day < 10 ? "0" + day: day}`
-    return dateString
-}
-/////////////////////
-listenToScrolling = () => window.addEventListener("scroll", this.loadNextPage);
-/////////////////////
-dontListenToScrolling = () => window.removeEventListener("scroll", this.loadNextPage);
-//////////////////////
-loadFirstPage = () => {
-    let firstPageUrl = `https://api.github.com/search/repositories?q=created:>${this.oneMonthBeforeToday()}&sort=stars&order=desc`
-    fetch(firstPageUrl)
-        .then(res => res.json())
-            .then(data => {
-                const repos = data.items;
-                this.setState({
-                        repos: repos,
-                        isLoadingFirstPage: false,
-                        currentPage: 1
-                    });
-                this.listenToScrolling();
-        })
-}
-///////////////////////
-loadNextPage = () => {
-    if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight){
-        this.dontListenToScrolling();
-        let currentPage = this.state.currentPage
-        let nextPageUrl = `https://api.github.com/search/repositories?q=created:>${this.oneMonthBeforeToday()}&sort=stars&order=desc${"&page="+(currentPage + 1)}`
-        fetch(nextPageUrl)
-            .then(res => res.json())
-                .then(data => {
-                    let {repos} = this.state;
-                    let nextPageRepos = data.items;
-                    this.setState({
-                            repos: repos.concat(nextPageRepos),
-                            currentPage: currentPage + 1
-                        })
-                    this.listenToScrolling();
-                })
-    }
-}
-///////////////////////
-componentWillUnmount(){
+    let dateString = `${year}-${
+      month < 9 && month !== 0 ? "0" + month : month === 0 ? 12 : month
+    }-${day < 10 ? "0" + day : day}`;
+    return dateString;
+  };
+  //
+  listenToScrolling = () =>
+    window.addEventListener("scroll", this.loadNextPage);
+  //
+  scrolledToTheEndOfList = () =>
+    window.innerHeight + document.documentElement.scrollTop ===
+    document.documentElement.offsetHeight;
+  //
+  dontListenToScrolling = () =>
     window.removeEventListener("scroll", this.loadNextPage);
-}
-//////////////////////
+  //
+  loadFirstPage = () => {
+    let firstPageUrl = `https://api.github.com/search/repositories?q=created:>${this.oneMonthBeforeToday()}&sort=stars&order=desc`;
+    fetch(firstPageUrl)
+      .then(res => res.json())
+      .then(data => {
+        const repos = data.items;
+        this.setState({
+          repos: repos,
+          isLoadingFirstPage: false,
+          currentPage: 1
+        });
+        this.listenToScrolling();
+      });
+  };
+  //
+  loadNextPage = () => {
+    if (this.scrolledToTheEndOfList()) {
+      this.dontListenToScrolling();
+      let currentPage = this.state.currentPage;
+      let nextPageUrl = `https://api.github.com/search/repositories?q=created:>${this.oneMonthBeforeToday()}&sort=stars&order=desc${"&page=" +
+        (currentPage + 1)}`;
+      fetch(nextPageUrl)
+        .then(res => res.json())
+        .then(data => {
+          let { repos } = this.state;
+          let nextPageRepos = data.items;
+          this.setState({
+            repos: repos.concat(nextPageRepos),
+            currentPage: currentPage + 1
+          });
+          this.listenToScrolling();
+        });
+    }
+  };
+  //
+  componentWillUnmount() {
+    this.dontListenToScrolling();
+  }
+
+  //
+  displayStarsAndIssuesAppropriatly = starsOrIsssues => {
+    if (starsOrIsssues > 1000) {
+      return (starsOrIsssues / 1000).toFixed(1) + "k";
+    } else {
+      return starsOrIsssues;
+    }
+  };
+
+  //
+  timePassedSinceLastTimePushed = pushed_at => {
+    const TODAY = new Date();
+    return TODAY - new Date(pushed_at);
+  };
+  //
+  convertAndDisplayTimeWithAppropriateUnit = pushed_at => {
+    if (
+      this.convertMillesecondsToHours(
+        this.timePassedSinceLastTimePushed(pushed_at)
+      ) >= 24
+    )
+      return (
+        this.convertMillesecondsToDays(
+          this.timePassedSinceLastTimePushed(pushed_at)
+        ) + " day(s) ago"
+      );
+    else if (
+      this.convertMillesecondsToMinutes(
+        this.timePassedSinceLastTimePushed(pushed_at)
+      ) >= 60
+    )
+      return (
+        this.convertMillesecondsToHours(
+          this.timePassedSinceLastTimePushed(pushed_at)
+        ) + " hour(s) ago"
+      );
+    else
+      return (
+        this.convertMillesecondsToMinutes(
+          this.timePassedSinceLastTimePushed(pushed_at)
+        ) + " minute(s) ago"
+      );
+  };
+  //
+  convertMillesecondsToMinutes = milleseconds => {
+    const A_MILLESECOND_IN_MINUTES = 1000 * 60;
+    return parseInt(milleseconds / A_MILLESECOND_IN_MINUTES);
+  };
+  //
+  convertMillesecondsToHours = milleseconds => {
+    const A_MILLESECOND_IN_HOURS = 1000 * 60 * 60;
+    return parseInt(milleseconds / A_MILLESECOND_IN_HOURS);
+  };
+  //
+  convertMillesecondsToDays = milleseconds => {
+    const A_MILLESECOND_IN_DAYS = 1000 * 60 * 60 * 24;
+    return parseInt(milleseconds / A_MILLESECOND_IN_DAYS);
+  };
+  //
   render() {
-    const {isLoadingFirstPage, repos} = this.state
-    const converToDays = 1000*60*60*24;
-    const converToHours = 1000*60*60;
-    const converToMinutes = 1000*60;
-    const today = new Date();
-    
+    const { isLoadingFirstPage, repos } = this.state;
+
     return (
       <div className="App">
-        {isLoadingFirstPage? "Loading.." :repos.map( repo =>
-        <RepoContainer
-            avatar = {repo.owner["avatar_url"]}
-            name = {repo.name}
-            description = {repo.description}
-            stars = {repo["stargazers_count"] > 1000? (repo["stargazers_count"]/ 1000).toFixed(1) + "k":repo["stargazers_count"]}
-            issues = {repo["open_issues_count"] > 1000? (repo["open_issues_count"]/ 1000).toFixed(1) + "k":repo["open_issues_count"]}
-            // this part was really fun :D!
-            timeInterval = {parseInt((today - new Date(repo["pushed_at"])) / converToHours) >= 24 ? parseInt((today - new Date(repo["pushed_at"])) / converToDays) + " day(s) ago": parseInt((today - new Date(repo["pushed_at"])) / converToMinutes) >= 60 ? parseInt((today - new Date(repo["pushed_at"])) / converToHours) + " hour(s) ago": parseInt((today - new Date(repo["pushed_at"])) / converToMinutes) + " minute(s) ago"}
-            userName = {repo.owner.login}
-            key = {repo.id}
-        />)}
+        {isLoadingFirstPage
+          ? "Loading.."
+          : repos.map(repo => (
+              <RepoContainer
+                avatar={repo.owner["avatar_url"]}
+                name={repo.name}
+                description={repo.description}
+                stars={this.displayStarsAndIssuesAppropriatly(
+                  repo["stargazers_count"]
+                )}
+                issues={this.displayStarsAndIssuesAppropriatly(
+                  repo["open_issues_count"]
+                )}
+                timeInterval={this.convertAndDisplayTimeWithAppropriateUnit(
+                  repo["pushed_at"]
+                )}
+                userName={repo.owner.login}
+                key={repo.id}
+              />
+            ))}
       </div>
     );
   }
